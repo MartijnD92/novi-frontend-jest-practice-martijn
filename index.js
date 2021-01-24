491a91320e7fc8f893edef8ac6940368e1d26d71
Martijn Doensen <martijn.doensen@live.nl>
2021-01-24 13:35:57 +0100

    Remove solution from main

--- index.js
+++ index.js
@@ -1,14 +1,7 @@
 function characterCount(string) {
-    let count = 0;
-    for (let i = 0; i < string.length; i++) {
-        if (string[i] !== " " && string[i] !== "\n") {
-            count++;
-        }
-    }
-    console.log(count);
-    return count;
+	// WRITE YOUR SOLUTION HERE!
 }
 
 module.exports = {
-    characterCount: characterCount
+	characterCount: characterCount,
 };
